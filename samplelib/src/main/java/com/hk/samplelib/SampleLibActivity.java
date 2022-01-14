f0896347618d95d83b0f104aeb75dc9ef2c7c5e0
Harish <harish.kotikalapudi@gmail.com>
2022-01-14 14:13:43 +0530

    Initial commit

---
+++ samplelib/src/main/java/com/hk/samplelib/SampleLibActivity.java
@@ -0,0 +1,14 @@
+package com.hk.samplelib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SampleLibActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_lib);
+    }
+}
